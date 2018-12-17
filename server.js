6f33b715501b428558b22eda707d79bfbd7aca11
LatoyaBogle <concordiae20@gmail.com>
2018-12-16 19:46:46 -0500

    test

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.static("public"));
 // Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI;
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI);
 
 
 
@@ -166,7 +166,7 @@ app.post("/articles/:id", function(req, res) {
 
 
 // Listen on port 3000
-/*app.listen(MONGODB_URI, function() {
+app.listen(MONGODB_URI, function() {
   console.log("App running on port 3000!");
-});*/
+});
 
