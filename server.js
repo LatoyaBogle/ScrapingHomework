2b30a056db5171e3eba170a7452ed9668d1bf2a5
LatoyaBogle <concordiae20@gmail.com>
2018-12-17 21:43:57 -0500

    files add

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 // Connect to the Mongo DB
-var uri = "mongodb://lbogle:bootcamp1984@ds237574.mlab.com:37574/heroku_fnp4s0pt";
+var uri = "mongodb://lbogle:bootcamp1984@ds237574.mlab.com:37574/heroku_fnp4s0pt"+ { useNewUrlParser: true };
 
 mongoose.connect(uri, function(err, db) {
   if(err) {
